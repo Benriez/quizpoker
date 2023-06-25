6b340118fd230092f6327f3e291c34a7d01282ad
Costa Weiland <costa.weiland@stud.tu-darmstadt.de>
2023-06-25 02:52:01 +0200

    implemented state management

--- lib/cubit/cubit.dart
+++ lib/cubit/cubit.dart
@@ -11,6 +11,8 @@ class UpdateValuesEvent extends MyEvent {
   UpdateValuesEvent(this.i, this.j, this.k);
 }
 
+class ChangeToBStateEvent extends MyEvent {}
+
 // Define the states
 class MyState {
   final int i;
@@ -20,14 +22,46 @@ class MyState {
   MyState(this.i, this.j, this.k);
 }
 
+class BState extends MyState {
+  BState(int i, int j, int k) : super(i, j, k);
+}
+
+class HState extends MyState {
+  HState(int i, int j, int k) : super(i, j, k);
+}
+
+class S0State extends MyState {
+  S0State() : super(0, 0, 0);
+}
+
+class EState extends S0State {
+  EState() : super();
+}
+
+// Define the cubit
 // Define the cubit
 class MyCubit extends Cubit<MyState> {
-  MyCubit() : super(MyState(0, 0, 0));
+  MyCubit() : super(S0State());
 
-  void updateValues(int i, int j, int k) {
-    // Perform any business logic or calculations based on the parameters
+  void bStateTransition(int i, int j, int k){
+    if (i < 3) {
+      emit(BState(i+1, j, k));
+    } else if (j <2 ){
+      emit(HState(i, j, k+1));
+    }
+    else emit(EState());
+  }
 
-    // Emit a new state
-    emit(MyState(i, j, k));
+  void hStateTransation(int i, int j, int k){
+    emit(BState(i , j+1, k));
+  }
+
+  void eStateTransition(){
+    emit(S0State());
+  }
+
+  void changeToBState() {
+    final currentState = state as S0State;
+    emit(BState(currentState.i, currentState.j, currentState.k));
   }
 }
